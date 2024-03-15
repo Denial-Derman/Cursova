cbd40a3e042497338af02cc009721a79e6ad5f22
Denial-Derman <denis.zavdoviev45@gmail.com>
2024-03-15 11:35:59 +0200

    add tasck1

--- js/timetable-admin.js
+++ js/timetable-admin.js
@@ -1,41 +1,60 @@
-function addFields() {
-   let container = document.getElementById("dynamicFields");
+function addFields(button) {
+   let container = button.parentElement.parentElement;
    let newFields = document.createElement("p");
    newFields.innerHTML = '<input type="time" name="times[]" id=""><input type="text" name="times_text[]" id="" placeholder="текст до пункту">';
    container.appendChild(newFields);
 }
+function removeField(button) {
+   let container = button.parentElement.parentElement;
+   if (container.childElementCount > 1) {
+      container.removeChild(container.lastElementChild);
+   }
+}
+function handleDayVar1Change() {
+   let dayVar1Select = document.getElementById("DayVar1");
+   let dayVar2Select = document.getElementById("DayVar2");
+   let dynamicFields2 = document.querySelector("#dynamicFields2 .time-input");
+   let dayVar3Select = document.getElementById("DayVar3");
+   let dynamicFields3 = document.querySelector("#dynamicFields3 .time-input");
+
+   if (dayVar1Select.value === "1") {
+      dayVar2Select.disabled = true;
+      dynamicFields2.style.display = "none";
+      dayVar3Select.disabled = true;
+      dynamicFields3.style.display = "none";
+   } else if (dayVar1Select.value === "2") {
+      dayVar2Select.disabled = true;
+      dynamicFields2.style.display = "none";
+      dayVar3Select.disabled = false;
+      dynamicFields3.style.display = "block";
+   } else if (dayVar1Select.value === "3") {
+      dayVar2Select.disabled = false;
+      dynamicFields2.style.display = "block";
+      dayVar3Select.disabled = false;
+      dynamicFields3.style.display = "block";
+      handleDayVar2Change();
+   }
+}
+
+function handleDayVar2Change() {
+   let dayVar2Select = document.getElementById("DayVar2");
+   let dayVar3Select = document.getElementById("DayVar3");
+   let dynamicFields3 = document.querySelector("#dynamicFields3 .time-input");
+
+   if (dayVar2Select.value === "1") {
+      dayVar3Select.disabled = true;
+      dynamicFields3.style.display = "none";
+   } else if (dayVar2Select.value === "2") {
+      dayVar3Select.disabled = false;
+      dynamicFields3.style.display = "block";
+   }
+}
+handleDayVar1Change();
 document.addEventListener('DOMContentLoaded', function () {
    const isSmallScreen = window.matchMedia('(max-width: 900px)').matches;
    let timetableContainer = document.querySelector('.timetable__flex');
-   // timetableContainer.addEventListener('click', function (event) {
-   //    if (event.target.classList.contains('timetable__btn')) {
-   //       let flexBlocks = document.querySelectorAll('.timetable__flex-block');
-   //       let flexBlockIndex = Array.from(flexBlocks).indexOf(event.target.closest('.timetable__flex-block'));
-   //       let imgBlock = flexBlocks[flexBlockIndex].querySelector('.timetable__img-block');
-   //       let timeBlock = flexBlocks[flexBlockIndex].querySelector('.timetable__time');
-   //       if (!isSmallScreen) {
-   //          if (imgBlock.style.width === '55%') {
-   //             imgBlock.style.width = '74%';
-   //             timeBlock.style.opacity = '0';
-   //             timeBlock.style.left = '-450px';
-   //          } else {
-   //             imgBlock.style.width = '55%';
-   //             timeBlock.style.opacity = '1';
-   //             timeBlock.style.left = '0px';
-   //          }
-   //       }
-   //       else {
-   //          if (imgBlock.style.height === '50%') {
-   //             imgBlock.style.height = '100%';
-   //             timeBlock.style.opacity = '0';
-   //             timeBlock.style.top = '-50px';
-   //          } else {
-   //             imgBlock.style.height = '50%';
-   //             timeBlock.style.opacity = '1';
-   //             timeBlock.style.top = '0px';
-   //          }
-   //       }
-   //    }
+   document.querySelector('.custom-file-upload').addEventListener('click', function () {
+      document.querySelector('.image-form').click();
+   });
 
-   // });
 });
